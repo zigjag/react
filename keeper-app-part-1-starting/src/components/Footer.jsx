54615dbc734a8af0acfbdc3c58ca970f81aaeaf8
Joseph Kligel <jkligel@gmail.com>
2020-05-29 23:59:39 -0500

    Using bash script to push all updates

---
+++ keeper-app-part-1-starting/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer(){
+    const date = new Date()
+    const year = date.getFullYear()
+
+    return(<footer><p>Copyright &copy; {year}</p></footer>)
+}
+
+export default Footer;
---
+++ keeper-app-part-1-starting/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note(){
+    return(
+        <div class='note'>
+            <h1>First Keeper Note</h1>
+            <p>Just installed the packages and ran the npm start command</p>
+        </div>
+    )
+}
+
+export default Note;
